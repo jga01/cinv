f630e9af29b94ec00576ba7b88702e06c6536f65
Tiago Goossen <tiagogoossenbra@gmail.com>
2024-04-24 19:59:30 -0300

    feat: add quantity slider and image drag&drop

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #define RAYLIB_NUKLEAR_IMPLEMENTATION
 #include "raylib-nuklear.h"
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 #define BUFF_SIZE 256
 
@@ -23,11 +23,12 @@ int main(int argc, char *argv[])
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "CINV");
 
     // Create the Nuklear Context
-    int fontSize = 10;
+    int fontSize = 12;
     struct nk_context *ctx = InitNuklear(fontSize);
 
     struct grid_item items[max_items];
-    char buf[BUFF_SIZE] = "Type some item to be added...";
+    char buf[BUFF_SIZE] = "Item to be added...";
+    char image_path[BUFF_SIZE] = "Path: none";
     int list_size = 0;
 
     struct nk_image remove_icon = LoadNuklearImage("../resources/remove.png");
@@ -71,40 +72,44 @@ int main(int argc, char *argv[])
                 if (nk_group_begin(ctx, "grid_header", row_flags))
                 {
                     nk_layout_row_dynamic(ctx, 0, 5);
-                    nk_label(ctx, "Image", NK_TEXT_ALIGN_LEFT);
-                    nk_label(ctx, "Name", NK_TEXT_ALIGN_LEFT);
-                    nk_label(ctx, "Quantity", NK_TEXT_ALIGN_LEFT);
+                    nk_label(ctx, "Image", NK_TEXT_LEFT);
+                    nk_label(ctx, "Name", NK_TEXT_LEFT);
+                    nk_label(ctx, "Quantity", NK_TEXT_LEFT);
                     nk_group_end(ctx);
                 }
 
-                nk_layout_row_dynamic(ctx, 50, 1);
+                nk_layout_row_dynamic(ctx, 70, 1);
                 for (int i = 0; i < list_size; i++)
                 {
                     if (nk_group_begin(ctx, "item row", row_flags | NK_WINDOW_BORDER))
                     {
-                        nk_layout_row_begin(ctx, NK_DYNAMIC, 40, 2);
-                        nk_layout_row_push(ctx, 0.8f);
+                        nk_layout_row_begin(ctx, NK_DYNAMIC, 60, 3);
+                        nk_layout_row_push(ctx, 0.2f);
+                        nk_image(ctx, *items[i].img);
+                        nk_layout_row_push(ctx, 0.6f);
                         if (nk_group_begin(ctx, "item values", row_flags))
                         {
                             char qty[10] = {0};
                             sprintf(qty, "%d", items[i].quantity);
 
-                            nk_layout_row_dynamic(ctx, 0, 3);
-                            nk_image(ctx, *items[i].img);
-                            nk_label(ctx, items[i].name, NK_TEXT_ALIGN_LEFT);
-                            nk_label(ctx, qty, NK_TEXT_ALIGN_LEFT);
+                            nk_layout_row_dynamic(ctx, 0, 2);
+                            nk_label(ctx, items[i].name, NK_TEXT_LEFT);
+                            nk_label(ctx, qty, NK_TEXT_LEFT);
                             nk_group_end(ctx);
                         }
                         nk_layout_row_push(ctx, 0.2f);
                         if (nk_group_begin(ctx, "item options", row_flags))
                         {
-                            nk_layout_row_dynamic(ctx, 30, 2);
+                            nk_layout_row_dynamic(ctx, 50, 2);
                             if (nk_button_image(ctx, edit_icon))
                             {
                                 strcpy(items[i].name, buf);
                             }
                             if (nk_button_image(ctx, remove_icon))
                             {
+                                UnloadNuklearImage(*items[i].img);
+                                free(items[i].img);
+                                items[i].img = 0;
                                 for (int j = i; j < max_items - 1; j++)
                                 {
                                     items[j] = items[j + 1];
@@ -122,15 +127,35 @@ int main(int argc, char *argv[])
             nk_layout_row_push(ctx, 0.3f);
             if (nk_group_begin(ctx, "panel", NK_WINDOW_NO_SCROLLBAR))
             {
+                int item_quantity;
+
                 nk_layout_row_dynamic(ctx, 0, 1);
                 nk_label(ctx, "Item:", NK_TEXT_LEFT);
                 nk_edit_string_zero_terminated(ctx, NK_EDIT_BOX | NK_EDIT_AUTO_SELECT, buf, sizeof(buf), nk_filter_default);
+                nk_label(ctx, "Quantity:", NK_TEXT_LEFT);
+                nk_property_int(ctx, "Units", 0, &item_quantity, 99999.0f, 1, 1);
+                nk_label(ctx, "Drop an image here!", NK_TEXT_LEFT);
+                nk_label(ctx, image_path, NK_TEXT_LEFT);
+                FilePathList droppedFiles = LoadDroppedFiles();
+                if (droppedFiles.count)
+                {
+                    strcpy(image_path, droppedFiles.paths[0]);
+                }
+                UnloadDroppedFiles(droppedFiles);
+
                 if (nk_button_label(ctx, "Add"))
                 {
                     if (list_size < max_items)
                     {
-                        struct grid_item item = {.img = &settings_icon, .name = {0}, .quantity = 0};
+                        struct grid_item item = {.img = 0, .name = {0}, .quantity = item_quantity};
                         strcpy(item.name, buf);
+
+                        if (image_path != NULL)
+                        {
+                            item.img = malloc(sizeof(struct nk_image));
+                            *item.img = LoadNuklearImage(image_path);
+                        }
+
                         items[list_size] = item;
                         list_size++;
                     }
